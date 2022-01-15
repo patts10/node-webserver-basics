ac1289cfae20463f0d62336396153d774eabcd98
Christian Mendoza <patts2810@gmail.com>
2022-01-15 12:13:48 -0500

    deploy app - 2021-02-15

--- app.js
+++ app.js
@@ -12,29 +12,29 @@ hbs.registerPartials( __dirname + '/views/partials');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.render('home', {
-    name: 'Christian Mendoza',
-    title: 'React Developer'
-  });
-});
+// app.get('/', (req, res) => {
+//   res.render('home', {
+//     name: 'Christian Mendoza',
+//     title: 'React Developer'
+//   });
+// });
 
-app.get('/generic', (req, res) => {
-  res.render('generic', {
-    name: 'Christian Mendoza',
-    title: 'React Developer'
-  });
-});
+// app.get('/generic', (req, res) => {
+//   res.render('generic', {
+//     name: 'Christian Mendoza',
+//     title: 'React Developer'
+//   });
+// });
 
-app.get('/elements', (req, res) => {
-  res.render('elements', {
-    name: 'Christian Mendoza',
-    title: 'React Developer'
-  });
-});
+// app.get('/elements', (req, res) => {
+//   res.render('elements', {
+//     name: 'Christian Mendoza',
+//     title: 'React Developer'
+//   });
+// });
 
 app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/public/404.html')
+  res.sendFile(__dirname + '/public/index.html')
 });
 
 app.listen(port, () => {
